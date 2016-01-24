e34698e7659714bb692e28bee1079b03c72b5442
Andy Ruland <aruland@gmail.com>
2016-01-25 01:19:16 +0400

    Replaced cart_id with user_id in DB

--- db/aswat_shop.sql
+++ db/aswat_shop.sql
@@ -2,7 +2,6 @@ CREATE TABLE "user" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "name" TEXT NOT NULL,
   "password" TEXT NOT NULL,
-  "cart_id" INTEGER DEFAULT NULL,
   "is_admin" INTEGER NOT NULL DEFAULT 0
 );
 
@@ -16,10 +15,11 @@ CREATE TABLE "product" (
 
 CREATE TABLE "cart" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "cart_id" INTEGER NOT NULL,
+  "user_id" INTEGER NOT NULL,
   "product_id" INTEGER NOT NULL,
   "quantity" INTEGER NOT NULL DEFAULT 1,
   FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE INDEX "cart_id_product_id" ON "cart" ("product_id");
