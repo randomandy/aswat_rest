06469d1e78eec155b68107823115092969dcf82a
Andy Ruland <aruland@gmail.com>
2016-01-24 16:37:53 +0400

    Added some SQLite mock data

---
+++ db/mock_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO "user" (name, password)  
+VALUES ('billgates', 'linuxrules');
+
+INSERT INTO "user" (name, password, is_admin)  
+VALUES ('linustorvalds', 'ilovexbox', 1);
+
+INSERT INTO "product" (name, stock)  
+VALUES ('The One Ring', 1);
+
+INSERT INTO "product" (name, stock)  
+VALUES ('Secret Death Star Plans', 5);
+
+INSERT INTO "product" (name, stock)  
+VALUES ('Piccards Chair', 2);
