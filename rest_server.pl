2157848246db7b20e2b11457cd6b9980e3d8c5ba
Andy Ruland <aruland@gmail.com>
2016-01-25 13:50:51 +0400

    Added new function to validate session and return user hash

--- rest_server.pl
+++ rest_server.pl
@@ -479,5 +479,37 @@ sub _is_authorized {
 	return $session->{user_id};
 }
 
+sub _get_authorized_user {
+	my ($session_token) = @_;
+
+	# Validate session token. Invalid if longer than MD5 32 chars
+	return
+		unless $session_token =~ m/^.{1,32}$/;
+
+	# Check if session token is valid/exists in db
+	my $sql = "SELECT id, user_id, datetime(created, 'localtime') AS created "
+		. 'FROM session WHERE token = ?';
+	my $session = $db->query($sql, $session_token)->hash;
+
+	return
+		unless $session->{id};
+
+	# Check if token is expired
+	my ($ss,$mm,$hh,$day,$month,$year) = strptime($session->{created});
+	my ($ss_now,$mm_now,$hh_now,$day_now,$month_now,$year_now) = localtime;
+
+	return
+		if $year != $year_now
+			|| $month != $month_now
+			|| $day != $day_now;
+
+	# Fetch all users from DB for later authentication
+	$sql = "SELECT id, name, password, is_admin FROM user "
+		. "WHERE id = ?";
+	my $user = $db->query($sql, $session->{user_id})->hash;	
+
+	return $user;
+}
+
 # Run the application
 $app->start;
